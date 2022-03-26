27b1bc709ee97864a0ac5af2d02d22218a5b07bf
dograkunal <kunaldogra31@gmail.com>
2022-03-26 19:20:38 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="head">Head data</header>
+      <h3 className="base_back">Here is some sample data....</h3>
+    </>
+  );
+}
+
+export default App;
